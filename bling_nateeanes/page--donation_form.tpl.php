a09401587d8afcdc96e354b60d5c3e5b073af320
Nathan Eanes <nathan@visualatte.com>
2014-03-29 23:43:37 -0400

    Changed theme name

--- bling_nateeanes/page--donation_form.tpl.php
+++ bling_nateeanes/page--donation_form.tpl.php
@@ -1,7 +1,7 @@
 <div class="gridwrap pagewrap cf head">
   <div class="grid-9">
     <div id="logo-top">
-      <a href="/"><img src="/sites/all/themes/bling/img/IRClogo_RGB_lrg.jpg" /></a>
+      <a href="/"><img src="/sites/all/themes/bling_nateeanes/img/IRClogo_RGB_lrg.jpg" /></a>
     </div><?php if ($title): print '<h1>'. $title .'</h1>'; endif; ?>
     <div id="donation-message">
       <p>On the ground in some of the world&#39;s most troubled places, the IRC helps people at their moment of greatest need &mdash; providing shelter, medical care, and safety. And we stay for as long as we are able to help in the rebuilding of lives and livelihoods.</p><p>&nbsp;</p><p><strong>Make a tax-deductible gift to help the IRC aid refugees around the world.</strong></p>
@@ -9,7 +9,7 @@
   </div>
 
   <div class="grid-3">
-    <img src="/sites/all/themes/bling/img/efficiency.png" />
+    <img src="/sites/all/themes/bling_nateeanes/img/efficiency.png" />
     <div id="image-top">
       <div id="donation-image-caption">
         More than ninety cents of every dollar we spend is used to support IRC programs.
@@ -24,13 +24,13 @@
     </div>
 
     <div class="grid-3 right">
-      <img src="/sites/all/themes/bling/img/onetime_sidepromo_challengeC.jpg" />
+      <img src="/sites/all/themes/bling_nateeanes/img/onetime_sidepromo_challengeC.jpg" />
     </div>
   </div><!--gridwrap-->
 
   <div class="gridwrap pagewrap cf foot">
     <div class="grid-4">
-      <img src="/sites/all/themes/bling/img/certifications.png" /><br />
+      <img src="/sites/all/themes/bling_nateeanes/img/certifications.png" /><br />
     </div>
 
     <div class="grid-6">
@@ -52,14 +52,15 @@
         East 42nd Street New York, NY 10168 USA <strong>|</strong> Phone: +1
         212 551 3000 <strong>|</strong> Donate: +1 855 9RESCUE
         <strong>|</strong> <a href="http://www.rescue.org/respecting-your-privacy" target="_blank">Privacy Policy</a>
-    </div>
+			</div>
 
-    <div class="footer-line"></div>
+			<div class="footer-line"></div>
 
-    <div class="footer-line">
-      IRC is a 501(c)(3) not-for-profit organization <strong>|</strong>
-      Copyright © International Rescue Committee, <?php echo date("Y"); ?>
-    </div>
+	    <div class="footer-line">
+	      IRC is a 501(c)(3) not-for-profit organization <strong>|</strong>
+	      Copyright © International Rescue Committee, <?php echo date("Y"); ?>
+	    </div>
+		</div>
   </div>
 
 </div><!--container-->
