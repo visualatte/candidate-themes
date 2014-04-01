241bda0072d9803987dc4d07a1100bcecb5ba76b
Nathan Eanes <nathan@visualatte.com>
2014-03-31 23:07:45 -0400

    Spaces instead of tabs and added comments.

--- bling_nateeanes/page--donation_form.tpl.php
+++ bling_nateeanes/page--donation_form.tpl.php
@@ -1,66 +1,66 @@
 <div class="gridwrap pagewrap cf head">
-	<div class="inner cf">
-		<div id="logo-top" class="first">
-			<a href="/"><img src="/sites/all/themes/candidate_themes/bling_nateeanes/img/IRClogo_160px.jpg" /></a>
-		</div>
-		
-		<div class="grid-3 last" id="header_right">
-			<img src="/sites/all/themes/candidate_themes/bling_nateeanes/img/efficiency.png" /><br />
-			More than ninety cents of every dollar we spend is used to support IRC programs.
-		</div>
-			
-		<div class="grid-6" id="donation-message">
-			<?php if ($title): print '<h1>'. $title .'</h1>'; endif; ?>
-			<p>On the ground in some of the world&#39;s most troubled places, the IRC helps people at their moment of greatest need &mdash; providing shelter, medical care, and safety. And we stay for as long as we are able to help in the rebuilding of lives and livelihoods.</p>
-			<p><strong>Make a tax-deductible gift to help the IRC aid refugees around the world.</strong></p>
-		</div>
-	</div><!-- /inner -->
+  <div class="inner cf">
+    <div id="logo-top" class="first">
+      <a href="/"><img src="/sites/all/themes/candidate_themes/bling_nateeanes/img/IRClogo_160px.jpg" /></a>
+    </div>
+    
+    <div class="grid-3 last" id="header_right">
+      <img src="/sites/all/themes/candidate_themes/bling_nateeanes/img/efficiency.png" /><br />
+      More than ninety cents of every dollar we spend is used to support IRC programs.
+    </div>
+
+    <div class="grid-6" id="donation-message">
+      <?php if ($title): print '<h1>'. $title .'</h1>'; endif; ?>
+      <p>On the ground in some of the world&#39;s most troubled places, the IRC helps people at their moment of greatest need &mdash; providing shelter, medical care, and safety. And we stay for as long as we are able to help in the rebuilding of lives and livelihoods.</p>
+      <p><strong>Make a tax-deductible gift to help the IRC aid refugees around the world.</strong></p>
+    </div>
+  </div><!-- /inner -->
 </div><!--/header gridwrap-->
 
 <div class="gridwrap pagewrap cf content">
-	<div class="inner cf">
-		<div class="grid-12 first last">
-			<?php if (!empty($tabs['#primary'])): 
-				print '<ul class="tabs primary">'. render($tabs) .'</ul>'; 
-			endif; ?>
-			<?php if ($show_messages && $messages): 
-				print $messages; 
-			endif; ?>
-			<div id="ratings_from">
-				<h2>The IRC has earned the highest ratings from:</h2>
-				<img src="/sites/all/themes/candidate_themes/bling_nateeanes/img/sidebar_ratings.jpg" />
-			</div>
-			<?php print render($page['content']); ?>
-		</div>
-	</div><!-- /inner -->
+  <div class="inner cf">
+    <div class="grid-12 first last">
+      <?php if (!empty($tabs['#primary'])): 
+        print '<ul class="tabs primary">'. render($tabs) .'</ul>'; 
+      endif; ?>
+      <?php if ($show_messages && $messages): 
+        print $messages; 
+      endif; ?>
+      <div id="ratings_from">
+        <h2>The IRC has earned the highest ratings from:</h2>
+        <img src="/sites/all/themes/candidate_themes/bling_nateeanes/img/sidebar_ratings.jpg" />
+      </div>
+      <?php print render($page['content']); ?>
+    </div>
+  </div><!-- /inner -->
 </div><!--gridwrap-->
 
 <div class="gridwrap pagewrap cf foot">
-	<div class="inner cf">
-		<div class="grid-4 first">
-			<img src="/sites/all/themes/candidate_themes/bling_nateeanes/img/certifications.png" /><br />
-		</div>
-			
-		<div class="grid-6 first last">
-			Of every $1 the IRC spends, more than 90 cents goes to programs and services that directly benefit refugees and war affected populations.<br /><br />
-			Donors living outside the US and Canada, please use our<br />
-			<a href="/donate/international">international donation form</a>
-		</div>
+  <div class="inner cf">
+    <div class="grid-4 first">
+      <img src="/sites/all/themes/candidate_themes/bling_nateeanes/img/certifications.png" /><br />
+    </div>
+
+    <div class="grid-6 first last">
+      Of every $1 the IRC spends, more than 90 cents goes to programs and services that directly benefit refugees and war affected populations.<br /><br />
+      Donors living outside the US and Canada, please use our<br />
+      <a href="/donate/international">international donation form</a>
+    </div>
   </div><!-- /inner -->
 </div><!--gridwrap-->
 
 <div class="container cf close">
-	<p class="footer-line">
-		<strong>International Rescue Committee</strong> <strong>|</strong> 122
-		East 42nd Street New York, NY 10168 USA <strong>|</strong> Phone: +1
-		212 551 3000 <strong>|</strong> Donate: +1 855 9RESCUE
-		<strong>|</strong> <a href="http://www.rescue.org/respecting-your-privacy" target="_blank">Privacy Policy</a>
-	</p>
-	
-	<p class="footer-line">
-		IRC is a 501(c)(3) not-for-profit organization <strong>|</strong>
-		Copyright &copy; International Rescue Committee, <?php echo date("Y"); ?>
-	</p>
+  <p class="footer-line">
+    <strong>International Rescue Committee</strong> <strong>|</strong> 122
+    East 42nd Street New York, NY 10168 USA <strong>|</strong> Phone: +1
+    212 551 3000 <strong>|</strong> Donate: +1 855 9RESCUE
+    <strong>|</strong> <a href="http://www.rescue.org/respecting-your-privacy" target="_blank">Privacy Policy</a>
+  </p>
+  
+  <p class="footer-line">
+    IRC is a 501(c)(3) not-for-profit organization <strong>|</strong>
+    Copyright &copy; International Rescue Committee, <?php echo date("Y"); ?>
+  </p>
 </div>
 
 </div><!--container-->
--- bling_nateeanes/template.php
+++ bling_nateeanes/template.php
@@ -6,67 +6,69 @@
  */
 
 function bling_nateeanes_form_alter(&$form, &$form_state, $form_id) {
-	$form['submitted']['billing_information']['payment_fields']['credit']['card_number']['#title'] = 'Credit Card Number:';
-	$form['submitted']['billing_information']['payment_fields']['credit']['card_number']['#description'] = '(no spaces)';
-	$form['submitted']['billing_information']['payment_fields']['credit']['card_number']['#required'] = TRUE;
-	$form['submitted']['billing_information']['payment_fields']['credit']['expiration_date']['#title'] = 'Expiration Date:';
-	$form['submitted']['billing_information']['payment_fields']['credit']['expiration_date']['card_expiration_month']['#required'] = TRUE;
-	$form['submitted']['billing_information']['payment_fields']['credit']['card_cvv']['#title'] = 'Card Security Code:';
-	$form['submitted']['billing_information']['payment_fields']['credit']['card_cvv']['#required'] = TRUE;
-	$form['submitted']['billing_information']['payment_fields']['credit']['card_cvv']['#description'] = '<a href="#">What\'s this?</a>';
+  $form['submitted']['billing_information']['payment_fields']['credit']['card_number']['#title'] = 'Credit Card Number:';
+  $form['submitted']['billing_information']['payment_fields']['credit']['card_number']['#description'] = '(no spaces)';
+  $form['submitted']['billing_information']['payment_fields']['credit']['card_number']['#required'] = TRUE;
+  $form['submitted']['billing_information']['payment_fields']['credit']['expiration_date']['#title'] = 'Expiration Date:';
+  $form['submitted']['billing_information']['payment_fields']['credit']['expiration_date']['card_expiration_month']['#required'] = TRUE;
+  $form['submitted']['billing_information']['payment_fields']['credit']['card_cvv']['#title'] = 'Card Security Code:';
+  $form['submitted']['billing_information']['payment_fields']['credit']['card_cvv']['#required'] = TRUE;
+  $form['submitted']['billing_information']['payment_fields']['credit']['card_cvv']['#description'] = '<a href="#">What\'s this?</a>';
 }
 
 /**
  * Implementation of theme_preprocess_html().
+ *
+ * Adds 'viewport' meta tag, conditional stylesheets for IE,
+ * and respond.js to make IE work with responsive design.
  */
 
 function bling_nateeanes_preprocess_html(&$vars) {
-	// Add the Meta Viewport tag.
-	$tag = array(
-	  '#tag' => 'meta', // The #tag is the html tag - <link />
-	  '#attributes' => array( // Set up an array of attributes inside the tag
-	    'name' => 'viewport', 
-	    'content' => 'width=device-width, initial-scale=1, maximum-scale=1',
-	  ),
-	);
-	drupal_add_html_head($tag, 'meta_viewport');
-	
-	// Add conditional stylesheets for IE.
-	$options = array(
-		'ie7' => array(
-	    'group' => CSS_THEME,
-	    'browsers' => array(
-	      'IE' => 'lte IE 7',
-	      '!IE' => FALSE,
-	    ),
-	    'weight' => 999,
-	    'every_page' => TRUE,
-	    'preprocess' => FALSE,
-	  ),
-	  'ie9' => array(
-	    'group' => CSS_THEME,
-	    'browsers' => array(
-	      'IE' => 'lte IE 9',
-	      '!IE' => FALSE,
-	    ),
-	    'weight' => 999,
-	    'every_page' => TRUE,
-	    'preprocess' => FALSE,
-	  )
-	);
+  // Add the Meta Viewport tag.
+  $tag = array(
+    '#tag' => 'meta',
+    '#attributes' => array(
+      'name' => 'viewport', 
+      'content' => 'width=device-width, initial-scale=1, maximum-scale=1',
+    ),
+  );
+  drupal_add_html_head($tag, 'meta_viewport');
+  
+  // Add conditional stylesheets for IE.
+  $options = array(
+    'ie7' => array(
+      'group' => CSS_THEME,
+      'browsers' => array(
+        'IE' => 'lte IE 7',
+        '!IE' => FALSE,
+      ),
+      'weight' => 999,
+      'every_page' => TRUE,
+      'preprocess' => FALSE,
+    ),
+    'ie9' => array(
+      'group' => CSS_THEME,
+      'browsers' => array(
+        'IE' => 'lte IE 9',
+        '!IE' => FALSE,
+      ),
+      'weight' => 999,
+      'every_page' => TRUE,
+      'preprocess' => FALSE,
+    )
+  );
   foreach ($options as $key => $option) {
-	  drupal_add_css(drupal_get_path('theme', 'bling_nateeanes') . '/' . $key . '-and-below.css', $option);
+    drupal_add_css(drupal_get_path('theme', 'bling_nateeanes') . '/' . $key . '-and-below.css', $option);
   }
-  //dsm($options);
-	
-	// Add Respond.js
-	$options = array(
-		'type' => 'file',
-		'cache' => FALSE,
-		'weight' => 9999,
-		'preprocess' => FALSE,
-	);
-	drupal_add_js(drupal_get_path('theme', 'bling_nateeanes') . '/js/respond.min.js', $options);
+  
+  // Add Respond.js
+  $options = array(
+    'type' => 'file',
+    'cache' => FALSE,
+    'weight' => 9999,
+    'preprocess' => FALSE,
+  );
+  drupal_add_js(drupal_get_path('theme', 'bling_nateeanes') . '/js/respond.min.js', $options);
 }
 
 /**
